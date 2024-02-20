70c845a346ece81b148dca29f1e9760417df3a42
HarisRasul12 <haris.rasul@mail.utoronto.ca>
2024-02-20 18:06:56 -0500

    renamed file

---
+++ SATreeCraft/classification_problems/fixed_height_tree_module.py
@@ -0,0 +1,228 @@
+# Created by: Haris Rasul
+# Date: November 19th 2023
+# Python script to build the complete tree and create literals
+# for a given depth and dataset. Will attempt to maximize the number of correct labels given to training dataset 
+# adding soft clauses for maximizing corrcet solution for a given depth and hard clauses 
+
+from pysat.formula import CNF
+from pysat.solvers import Solver
+from pysat.formula import WCNF
+from pysat.examples.rc2 import RC2
+from graphviz import Digraph
+import numpy as np
+from classification_problems.min_height_tree_module import *
+
+# Define the function to create literals based on the tree structure
+def create_literals_fixed_tree(TB, TL, F, C, dataset_size):
+    """
+    Create the literals for the SAT solver based on the tree structure and dataset size.
+
+    This function creates four types of literals:
+    - 'a' literals for feature splits at branching nodes,
+    - 's' literals for data points directed to left or right,
+    - 'z' literals for data points that end up at a leaf node,
+    - 'g' literals for assigning class labels to leaf nodes.
+    - 'p' literals for checking of data point was assigned correct label or not 
+
+    Parameters:
+    - TB (list): Indices of branching nodes in the tree.
+    - TL (list): Indices of leaf nodes in the tree.
+    - num_features (int): The number of features in the dataset.
+    - labels (list): The list of class labels for the dataset.
+    - dataset_size (int): The number of data points in the dataset.
+
+    Returns:
+    - literals (dict): A dictionary where keys are literal names and values are their corresponding indices for the SAT solver.
+    """
+
+    literals = {}
+    current_index = 1
+
+    # Create 'a' literals for feature splits at branching nodes
+    for t in TB:
+        for j in F:
+            literals[f'a_{t}_{j}'] = current_index
+            current_index += 1
+
+    # Create 's' literals for data points directed left or right at branching nodes
+    for i in range(dataset_size):
+        for t in TB:
+            literals[f's_{i}_{t}'] = current_index
+            current_index += 1
+
+    # Create 'z' literals for data points ending up at leaf nodes
+    for i in range(dataset_size):
+        for t in TL:
+            literals[f'z_{i}_{t}'] = current_index
+            current_index += 1
+
+    # Create 'g' literals for labels at leaf nodes
+    for t in TL:
+        for c in C:
+            literals[f'g_{t}_{c}'] = current_index
+            current_index += 1
+
+    # Create 'p' literals for checking correct label association given to training data point
+    for i in range(dataset_size):
+        literals[f'p_{i}'] = current_index
+        current_index += 1
+
+    return literals
+
+def build_clauses_fixed_tree(literals, X, TB, TL, num_features, labels,true_labels):
+    """
+    Constructs the clauses for the SAT solver based on the decision tree encoding. Now includes MAX SOLVER PROBLEM FOR FIXED HEIGHT 
+
+    Args:
+        literals (dict): A dictionary mapping literals to variable indices.
+        X (list): The dataset, a list of tuples representing data points.
+        TB (list): Indices of branching nodes.
+        TL (list): Indices of leaf nodes.
+        num_features (int): Number of features in the dataset.
+        labels (list): Possible class labels for the data points.
+
+    Returns:
+        WCNF: A WCNF object containing all the clauses, with hard clauses for the tree structure and soft clauses for maximizing correctly classified points
+    """
+    # Now the problem has become Partial MaxSAT - we will assign weights to the soft clauses Eq. (13). Eq(1-10,12) HARD clauses 
+    wcnf = WCNF()
+    
+    # Clause (1) and (2): Feature selection at branching nodes
+    for t in TB:
+        # At least one feature is chosen (Clause 2)
+        clause = [literals[f'a_{t}_{j}'] for j in range(num_features)]
+        wcnf.append(clause)
+        
+        # No two features are chosen (Clause 1)
+        for j in range(num_features):
+            for jp in range(j + 1, num_features):
+                clause = [-literals[f'a_{t}_{j}'], -literals[f'a_{t}_{jp}']]
+                wcnf.append(clause)
+
+    # Clause (3) and (4): Data point direction based on feature values
+    for j in range(num_features):
+        Oj = compute_ordering(X, j)
+        for (i, ip) in Oj:
+            if X[i][j] < X[ip][j]:  # Different feature values (Clause 3)
+                for t in TB:
+                    wcnf.append([-literals[f'a_{t}_{j}'], literals[f's_{i}_{t}'], -literals[f's_{ip}_{t}']])
+            if X[i][j] == X[ip][j]:  # Equal feature values (Clause 4)
+                for t in TB:
+                    wcnf.append([-literals[f'a_{t}_{j}'], literals[f's_{i}_{t}'], -literals[f's_{ip}_{t}']])
+                    wcnf.append([-literals[f'a_{t}_{j}'], -literals[f's_{i}_{t}'], literals[f's_{ip}_{t}']])
+
+    # Clause (5 and 6): Path valididty form right traversla and left traversal 
+    for t in TL:
+        left_ancestors = get_ancestors(t, 'left')
+        right_ancestors = get_ancestors(t, 'right')
+        for i in range(len(X)):
+            # Data point i ends at leaf node t (Clause 5 and 6) - assumption made!!!
+            if left_ancestors:
+                wcnf.append([-literals[f'z_{i}_{t}']] + [literals[f's_{i}_{a}'] for a in left_ancestors])
+            if right_ancestors:
+                wcnf.append([-literals[f'z_{i}_{t}']] + [-literals[f's_{i}_{a}'] for a in right_ancestors])
+
+    # Clause (7): Each data point that does not end up in leaf node t has at least one deviation from the path
+    for xi in range(len(X)):
+        for t in TL:
+            deviations = []
+            left_ancestors = get_ancestors(t, 'left')  # Get left ancestors using TB indices
+            right_ancestors = get_ancestors(t, 'right')  # Get right ancestors using TB indices
+            # Only append deviations if there are ancestors on the corresponding side
+            if left_ancestors:
+                deviations.extend([-literals[f's_{xi}_{ancestor}'] for ancestor in left_ancestors])
+            if right_ancestors:
+                deviations.extend([literals[f's_{xi}_{ancestor}'] for ancestor in right_ancestors])
+            # Only append the clause if there are any deviations
+            if deviations:
+                wcnf.append([literals[f'z_{xi}_{t}']] + deviations)    
+
+    # Clause (8): Each leaf node is assigned at most one label
+    for t in TL:
+        for c in range(len(labels)):
+            for cp in range(c + 1, len(labels)):
+                wcnf.append([-literals[f'g_{t}_{labels[c]}'], -literals[f'g_{t}_{labels[cp]}']])
+
+    # Clause (9) and (10): Redundant constraints to prune the search space
+    # These clauses are optimizations
+    for t in TB:
+        # Find the data point with the lowest and highest feature value for each feature
+        for j in range(num_features):
+            sorted_by_feature = sorted(range(len(X)), key=lambda k: X[k][j])
+            lowest_value_index = sorted_by_feature[0]
+            highest_value_index = sorted_by_feature[-1]
+
+            # Clause (9): The data point with the lowest feature value is directed left
+            wcnf.append([-literals[f'a_{t}_{j}'], literals[f's_{lowest_value_index}_{t}']])
+
+            # Clause (10): The data point with the highest feature value is directed right
+            wcnf.append([-literals[f'a_{t}_{j}'], -literals[f's_{highest_value_index}_{t}']])
+
+    # New Hard Clause (12) for ensuring pi is true only when xi ends up in a leaf node with the correct label, REMOVED (CLAUSE 11)
+    for i, xi in enumerate(X):
+        for t in TL:
+            label = true_labels[i]
+            # This adds the clause (¬pi ∨ ¬zi,t ∨ gt,γ(xi))
+            wcnf.append([-literals[f'p_{i}'], -literals[f'z_{i}_{t}'], literals[f'g_{t}_{label}']])
+            
+    # Add the soft clauses (13) for each data point being correctly classified
+    for i in range(len(X)):
+        wcnf.append([literals[f'p_{i}']], weight=1)
+
+    return wcnf
+
+def solve_wcnf(wcnf, literals, TL, tree_structure, labels,features,datasetX):
+    """
+    Attempts to solve the given CNF using a SAT solver.
+
+    If a solution is found, it updates the tree structure with the correct labels for leaf nodes.
+
+    Args:
+    - cnf (CNF): The CNF object containing all clauses for the SAT solver.
+    - literals (dict): A dictionary mapping literals to variable indices.
+    - TL (list): Indices of leaf nodes in the tree.
+    - tree_structure (list): The complete binary tree structure.
+    - labels (list): The list of class labels for the dataset.
+
+    Returns:
+    - solution (list or str): The solution to the MaxSAT problem if it exists, otherwise "No solution exists".
+    """
+    with RC2(wcnf) as m:
+        model = m.compute()
+        cost = m.cost 
+    
+    #print(model)
+    if model:
+        # Update the tree structure with the correct labels for leaf nodes
+        for t in TL:
+            for label in labels:
+                if literals[f'g_{t}_{label}'] in model:
+                    tree_structure[t]['label'] = label
+                    break
+         # Set details for branching nodes
+        set_branch_node_features(model, literals, tree_structure,features,datasetX)
+        return model, cost
+    else:
+        return "No solution exists"
+
+def find_fixed_depth_tree(features, labels, true_labels_for_points, dataset,depth):
+    solution = "No solution exists"
+    tree_with_thresholds = None
+    tree = None
+    literals = None
+    cost = None
+
+    tree, TB, TL = build_complete_tree(depth)
+    literals = create_literals_fixed_tree(TB, TL, features, labels, len(dataset))
+    wcnf = build_clauses_fixed_tree(literals, dataset, TB, TL, len(features), labels, true_labels_for_points)
+    solution,cost = solve_wcnf(wcnf, literals, TL, tree, labels, features, dataset)
+    
+    if solution != "No solution exists":
+        tree_with_thresholds = add_thresholds(tree, literals, solution, dataset)
+        dot = visualize_tree(tree_with_thresholds)
+        dot.render(f'images/fixed_height/binary_decision_tree_fixed_depth_{depth}', format='png', cleanup=True)
+    else:
+        print('could not find solution')
+        return 'No solution'
+    
+    return tree_with_thresholds, literals, depth, solution, cost
---
+++ SATreeCraft/utils.py
@@ -0,0 +1,249 @@
+# Created by: Haris Rasul
+# Date: December 22nd 2023
+# Python script to convert datasets into proper formats for inputting into tree creation
+# Includes data loaders, sklearn frameworks data preporcessing in rder to feed into tree methods.
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import os
+
+#To do handle xlsx files, csv files, label on first index, label on last index - give in the exetnesions do the proessing return the object type with all the stuff
+class TreeDataLoaderBinaryNumerical:
+    """
+    A class to represent a dataset with labeled data for numerical and binary data only for features
+
+    Attributes:
+        file_path (str): The path to the dataset file.
+        delimiter (str): The delimiter used in the dataset file to separate columns.
+        label_position (int): The index of the column containing the labels. By default, it is set to -1, assuming the label is in the last column.
+        features (np.ndarray): The names of the features, encoded as strings representing their column index.
+        labels (np.ndarray): The unique labels present in the dataset after processing.
+        true_labels_for_points (np.ndarray): The array containing the labels for each data point after processing.
+        dataset (np.ndarray): The array containing the features for each data point.
+
+    Methods:
+        process_data_into_tree_form(): Reads the dataset from the file path, processes it, and populates the attributes with the processed data.
+
+    Usage:
+        # To create an instance of the class:
+        my_dataset = TreeDataLoaderMinHeightBinaryNumerical('path/to/your/datafile.csv', delimiter=',', label_position=-1)
+
+        # Access the processed data:
+        my_dataset.features
+        my_dataset.labels
+        my_dataset.true_labels_for_points
+        my_dataset.dataset
+    """
+    def __init__(self, file_path, delimiter=None, label_position=-1, custom_exclude=None):
+        self.file_path = file_path
+        self.delimiter = delimiter or ','
+        self.label_position = label_position
+        self.custom_exclude = custom_exclude or []
+        self.features = None
+        self.labels = None
+        self.true_labels_for_points = None
+        self.dataset = None
+        self.label_encoder = LabelEncoder()
+        self.process_data_into_tree_form()
+
+    def process_data_into_tree_form(self):
+        file_extension = os.path.splitext(self.file_path)[1].lower()
+
+        if file_extension in ['.csv', '.xls', '.xlsx']:
+            self._process_csv_or_excel()
+        else:
+            self._process_text_file()
+
+    def _process_csv_or_excel(self):
+        if self.file_path.endswith('.csv'):
+            df = pd.read_csv(self.file_path)
+        else:  # Excel file
+            df = pd.read_excel(self.file_path)
+
+        # Exclude custom columns if necessary
+        if self.custom_exclude:
+            df.drop(df.columns[self.custom_exclude], axis=1, inplace=True)
+
+        # Assign features and labels based on label position
+        if self.label_position is not None and self.label_position != -1:
+            features_array = df.drop(df.columns[self.label_position], axis=1).values
+            labels_array = df.iloc[:, self.label_position].values
+        else:
+            features_array = df.iloc[:, :-1].values
+            labels_array = df.iloc[:, -1].values
+
+        self._finalize_data(features_array, labels_array)
+
+    def _process_text_file(self):
+        features_list = []
+        raw_labels_list = []
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                components = line.strip().split(self.delimiter)
+                
+                # Extract label based on the label_position after excluding custom columns
+                label = components.pop(self.label_position) if self.label_position is not None else components.pop(-1)
+                raw_labels_list.append(label)
+                # print(label)
+                if len(self.custom_exclude) > 0:
+                    for i in self.custom_exclude:
+                        components.pop(i)
+                # Convert remaining components to float
+                try:
+                    features = [float(comp) for comp in components]
+                except ValueError as e:
+                    # Handle conversion error if necessary
+                    print(f"Error converting to float: {e}")
+                    continue  # Skip this line and continue with the next
+
+                features_list.append(features)
+
+        self._finalize_data(np.array(features_list), raw_labels_list)
+
+    def _finalize_data(self, features_array, labels_array):
+        # Encode labels to numeric values
+        self.true_labels_for_points = self.label_encoder.fit_transform(labels_array)
+        self.dataset = features_array
+        self.features = np.array([str(i) for i in range(self.dataset.shape[1])])
+        self.labels = np.unique(self.true_labels_for_points)
+
+class TreeDataLoaderWithCategorical:
+    """
+    A class to load and preprocess data from a text file for use in decision tree algorithms.
+    
+    This class handles both categorical and numerical data and converts text-based labels to 
+    numerical labels, while also handling rows with missing values.
+
+    Attributes:
+        file_path (str): The file path to the dataset.
+        label_index (int): The index of the column containing the labels.
+        numerical_indices (list of int): The indices of columns that contain numerical data.
+        categorical_string_index (int or None): The index where a single string of categorical 
+                                                 features is located, if applicable.
+        delimiter (str): The delimiter used in the text file to separate data columns.
+        dataset (np.ndarray): The array containing the processed features for each data point.
+        features_categorical (np.ndarray): The array containing the processed categorical features.
+        features_numerical (np.ndarray): The array containing the processed numerical features.
+        true_labels_for_points (np.ndarray): The array containing the processed labels for each data point.
+        labels (np.ndarray): The array containing the unique labels present in the dataset.
+        features (np.ndarray): The array containing the names of the features.
+
+    Methods:
+        process_data(): Main method to load and process the data from the file path.
+        
+    Usage:
+        # To create an instance of the class:
+        data_loader = TreeDataLoaderWithCategorical(
+            file_path='path/to/datafile.txt',
+            label_index=-1,
+            numerical_indices=[1, 2],
+            categorical_string_index=3
+        )
+
+        # Access the processed data:
+        data_loader.dataset
+        data_loader.features_categorical
+        data_loader.features_numerical
+        data_loader.true_labels_for_points
+        data_loader.labels
+        data_loader.features
+    """
+    def __init__(self, file_path, label_index, numerical_indices=None, categorical_feature_index=None, delimiter=','):
+        self.file_path = file_path
+        self.label_index = label_index
+        self.numerical_indices = numerical_indices
+        self.categorical_feature_index = categorical_feature_index
+        self.delimiter = delimiter
+        self.features = None
+        self.features_categorical = None
+        self.features_numerical = None
+        self.labels = None
+        self.true_labels_for_points = None
+        self.dataset = None
+        self.label_encoder = LabelEncoder()
+        self.process_data()
+    
+
+    def process_data(self):
+
+        
+        if self.categorical_feature_index != None:
+            with open(self.file_path, 'r') as file:
+                raw_data = [line.strip().split(self.delimiter) for line in file if '?' not in line]
+            
+            # Extract labels and encode them
+            labels = [row[self.label_index] for row in raw_data]
+            self.true_labels_for_points = self.label_encoder.fit_transform(labels)
+            self.labels = np.unique(self.true_labels_for_points)
+
+            # Process features
+            if self.categorical_feature_index is not None:
+                # Split the string at the categorical feature index into individual characters
+                features = [list(row[self.categorical_feature_index].strip()) for row in raw_data]
+            else:
+                # Treat each comma-separated value as a separate feature, excluding the label
+                features = [row[:self.label_index] + row[self.label_index+1:] for row in raw_data]
+
+            self.dataset = np.array(features)
+
+            # Generate feature names
+            self.features = np.array([str(i) for i in range(self.dataset.shape[1])])
+                
+            
+            # Identify and separate numerical and categorical features
+            if self.numerical_indices is not None:
+                self.features_numerical = self.dataset[:, self.numerical_indices].astype(float)
+                categorical_indices = list(set(range(self.dataset.shape[1])) - set(self.numerical_indices))
+                self.features_categorical = np.array([str(i) for i in categorical_indices])
+            else:
+                self.features_numerical = np.array([], dtype=float).reshape(self.dataset.shape[0], 0)
+                self.features_categorical = np.array([str(i) for i in range(self.dataset.shape[1])])
+        else:
+            # Initialize lists to hold the dataset and labels
+            dataset = []
+            labels = []
+
+            # Dictionary to map textual labels to numeric labels
+            label_mapping = {}
+            label_counter = 0
+
+            # Read the file
+            with open(self.file_path, 'r') as file:
+                for line in file:
+                    # Skip if there's a missing value
+                    if '?' in line:
+                        continue
+        
+                    # Split the line into parts and extract the label and features
+                    parts = line.strip().split(',')
+                    label = parts[self.label_index]
+                    parts.pop(self.label_index)
+                    features = parts
+                    
+                    # If the label is new, add it to the label mapping
+                    if label not in label_mapping:
+                        label_mapping[label] = label_counter
+                        label_counter += 1
+                    
+                    # Add the numeric label and features to their respective lists
+                    labels.append(label_mapping[label])
+                    dataset.append(features)
+
+            # Convert lists to numpy arrays
+            self.dataset = np.array(dataset, dtype=str)  # Assuming features are numeric
+            self.true_labels_for_points = np.array(labels, dtype=int)
+            # print(self.dataset)
+            features_list = [str(i) for i in range(self.dataset.shape[1])]
+            self.features = np.array(features_list)
+            self.labels = np.unique(self.true_labels_for_points)
+
+            if (self.numerical_indices is None):
+                self.features_numerical = np.array([])
+                self.features_categorical = self.features
+            else:
+                self.features_numerical = self.features[self.numerical_indices]
+                # Get the residual elements
+                mask = np.ones(len(self.features), dtype=bool)  # Create a mask of all True values
+                mask[self.numerical_indices] = False  # Set the indices in x to False
+                self.features_categorical = self.features[mask]  # Y contains el
